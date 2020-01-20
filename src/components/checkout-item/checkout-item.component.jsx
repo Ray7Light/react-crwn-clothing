433256c73ceda623c435d632d58b51535cb5c288
Marcin <m.ficek@infomex.pl>
2020-01-20 14:36:05 +0100

    + redux persist, collections routing

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -26,7 +26,7 @@ const CheckoutItem = ({ addItem, cartItem, clearItem, removeItem }) => {
           &#10095;
         </div>
       </span>
-      <span className="price">{price}</span>
+      <span className="price">${price}</span>
       <div className="remove-button" onClick={() => clearItem(cartItem)}>
         &#10005;
       </div>
