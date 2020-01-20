b8ff14e6071f0acccbf37f4411c585d85d400742
Marcin <m.ficek@infomex.pl>
2020-01-20 15:16:22 +0100

    + stripe integration, favicon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_ulyQyRCTXTyYGPOmRBUbuk3o00JKGcdPkP";
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://sendeyo.com/up/d/f3eb2117da"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+
+  function onToken(token) {
+    console.log(token);
+    alert("Payment Successful!");
+  }
+};
+
+export default StripeCheckoutButton;
