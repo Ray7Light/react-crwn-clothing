2123ae9438e094943172c54f1c98990048ed98d4
Marcin <m.ficek@infomex.pl>
2020-01-22 09:27:23 +0100

    + container pattern

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,34 @@
 import ShopActionTypes from './shop.types';
 
-export const updateCollections = collectionsMap => ({
-    type: ShopActionTypes.UPDATE_COLLECTIONS,
+import {
+    convertCollectionsSnapshotToMap,
+    firestore
+} from "../../firebase/firebase.utils";
+
+export const fetchCollectionsStart = () => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_START
+});
+
+export const fetchCollectionsSuccess = collectionsMap => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
     payload: collectionsMap
 });
+
+export const fetchCollectionsFailure = errorMsg => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+    payload: errorMsg
+});
+
+export const fetchCollectionsStartAsync = () => {
+    return dispatch => {
+        dispatch(fetchCollectionsStart());
+
+        firestore
+            .collection("collections")
+            .get()
+            .then(snapshot => {
+                const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+                dispatch(fetchCollectionsSuccess(collectionsMap))
+            }).catch(error => dispatch(fetchCollectionsFailure(error.message)))
+    }
+}
